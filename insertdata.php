416283081e3ebca7d84360639ce0e5a04ef556c5
filaakriz <filaakriz@gmail.com>
2022-09-12 16:34:59 +0800

    Update insertdata.php

--- insertdata.php
+++ insertdata.php
@@ -19,9 +19,9 @@ if ($conn->connect_error) {
 }
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime), branch_name (varchar 255)
-$user = INSERT INTO users(id,name,email,token,created,branch_name) VALUES (1818,'fila akriz','filaakriz@gmail.com','2fec392304a5c23ac138da22847f9b7c',now(),'filaakriz');
+$user = "INSERT INTO users(id,name,email,token,created,branch_name) VALUES (1818,'fila akriz','filaakriz@gmail.com','2fec392304a5c23ac138da22847f9b7c',now(),'filaakriz')";
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255), branch_name (varchar 255)
-$location = INSERT INTO locations(id,user_id,district,state,country,branch_name) VALUES (18,08,'seri kembangan','selangor','malaysia','filaakriz');
+$location = "INSERT INTO locations(id,user_id,district,state,country,branch_name) VALUES (18,08,'seri kembangan','selangor','malaysia','filaakriz')";
 
 
 
